10b80ef8b536b193a1bd484c7eb6339164bb8eb3
tobiasperel <74215065+tobiasperel@users.noreply.github.com>
2022-10-28 12:15:07 -0300

    Update CreateToken.sol

--- desafio1/contracts/CreateToken.sol
+++ desafio1/contracts/CreateToken.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CreateToken is ERC20 {
