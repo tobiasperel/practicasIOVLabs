657aa037ab12fd4385c6cde73b2d19792a303ca6
TobiPerel <pereltobias@gmail.com>
2022-10-29 19:11:03 -0300

    crate nft

--- desafio1/test/CreateToken.js
+++ desafio1/test/CreateToken.js
@@ -8,7 +8,7 @@ describe("CreateToken", function () {
         const CreateToken = await CreateTokenFactory.deploy();
         await CreateToken.deployed();
         expect(await CreateToken.balanceOf(deployer.address)).equal(100);
-        CreateToken.transfer("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", 22);
+        CreateToken.transfer("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", 22);
         CreateToken.transfer("0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", 10);
         CreateToken.transfer("0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", 37);
         expect(await CreateToken.balanceOf(deployer.address)).equal(31);
---
+++ desafioCreateNFT/test/CreateNFT.js
@@ -0,0 +1,25 @@
+const {ethers} = require("hardhat");
+const assert = require("assert");
+const {expect} = require("chai");
+const { exitCode } = require("process");
+describe("CreateNFT", function () {
+  it('has initial message', async  () => {
+    const [deployer] = await ethers.getSigners();
+    const CreateNFTFactory = await ethers.getContractFactory("CreateNFT");
+    const CreateNFT = await CreateNFTFactory.deploy();
+    await CreateNFT.deployed();
+    await CreateNFT.safeMint(deployer.address, 1);
+    // Chequeos
+    expect (await CreateNFT.owner()).to.equal(deployer.address); //Ser el dueño del contrato
+    expect (await CreateNFT.symbol()).to.equal("RF"); //Que el simbolo sea RF
+    expect (await CreateNFT.ownerOf(1)).to.equal(deployer.address); //Que el dueño del token sea el deployer
+    expect (await CreateNFT.balanceOf(deployer.address)).to.equal(1); //Que el balance del deployer sea 1
+
+    await CreateNFT.transferFrom(deployer.address, deployer.address, 1); //Que el deployer pueda transferir el token a si mismo
+    await CreateNFT.transferFrom(deployer.address, "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db", 1); //Que el deployer pueda transferir a otro
+
+    //chequeos
+    expect (await CreateNFT.ownerOf(1)).to.equal("0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"); //Que el  nuevo dueño del token realmente lo tenga
+    expect (await CreateNFT.balanceOf(deployer.address)).to.equal(0); //Que el balance del deployer sea 0
+  });
+});
