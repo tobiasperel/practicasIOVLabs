fe3f074df0a26307bc4440daa76f9eff5c164abb
Mati Rapoport <mati.rapoport@gmail.com>
2022-11-01 15:47:09 -0300

    Update HelloWorld.js

--- desafio0/test/HelloWorld.js
+++ desafio0/test/HelloWorld.js
@@ -4,7 +4,7 @@ const assert = require("assert");
 describe("HelloWorld", function () {
     it('has initial message', async  () => {
         const HelloWorldFactory = await ethers.getContractFactory("HelloWorld");
-        msg = "Hello mundo profencional" 
+        msg = "Hello mundo profencionallll" 
         const helloWorld = await HelloWorldFactory.deploy(msg);
         await helloWorld.deployed();
         assert.equal(await helloWorld.getMessage(),msg);
